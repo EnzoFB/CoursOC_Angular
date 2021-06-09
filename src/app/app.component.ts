8292bf16a89656deacb18c50c6b002ec6ef23efe
EnzoFB <enzo.ferreirabastos@gmail.com>
2021-06-09 18:31:48 +0200

    Gérez des données dynamiques

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  isAuth = false;
   title = 'Projet Angular';
+
+  appareilOne = "Machine à laver";
+  appareilTwo = "Frigo";
+  appareilThree = "Ordinateur";
+
+  constructor() {
+    setTimeout( () => { this.isAuth = true; }, 4000 );
+  }
+
+  onAllumer() {
+    console.log("On allume tout !")
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MonPremierComponent } from './mon-premier/mon-premier.component';
+import { AppareilComponent } from './appareil/appareil.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MonPremierComponent
+    MonPremierComponent,
+    AppareilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
