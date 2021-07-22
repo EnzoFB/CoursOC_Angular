aca3c2d58ac8be388eb148ffbed1c59b225bd00b
EnzoFB <enzo.ferreirabastos@gmail.com>
2021-07-22 22:29:56 +0200

    Chap. routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,22 @@ import { FormsModule } from '@angular/forms';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostListItemComponent } from './post-list/post-list-item/post-list-item.component';
 import { AppareilService } from './services/appareil.service';
+import { AuthService } from './services/auth.services';
 import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { AuthGuard } from './services/auth-guard.service';
 
 const appRoutes: Routes = [
-  { path: 'appareils', component: AppareilViewComponent },
+  { path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent },
+  { path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent },
   { path: 'auth', component: AuthComponent },
-  { path: '', component: AppareilViewComponent }
+  { path: '', component: AppareilViewComponent },
+  { path: 'not-found', component: FourOhFourComponent },
+  { path: '**', redirectTo: 'not-found' }
+  
 
 ];
 @NgModule({
@@ -26,7 +34,9 @@ const appRoutes: Routes = [
     PostListComponent,
     PostListItemComponent,
     AuthComponent,
-    AppareilViewComponent
+    AppareilViewComponent,
+    SingleAppareilComponent,
+    FourOhFourComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +44,9 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
-    AppareilService
+    AppareilService,
+    AuthService,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
